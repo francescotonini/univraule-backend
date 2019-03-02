a031f33a6502eca748dceca5aae50a229c3cd43b
Francesco Tonini <francescoantoniotonini@gmail.com>
2019-03-02 21:54:08 +0100

    Switch to Koa and pino

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-const bole   = require('bole');
+const pino   = require('pino')();
 const config = require('./lib/config');
 const server = require('./lib/server.js');
 
-// Initialize the logger
-bole.output([{ level: config('logLevel'), stream: process.stdout }]);
-
-const logger = bole('index');
 process.on('uncaughtException', (err) => {
-	logger.error(err);
+	pino.error(err);
 	process.exit(1);
 });
 
 // Starts the server
 server.listen(config(['server', 'port']), () => {
-	logger.info(`Listening on port ${config(['server', 'port'])}`);
+	pino.info(`Listening on port ${config(['server', 'port'])}`);
 });
